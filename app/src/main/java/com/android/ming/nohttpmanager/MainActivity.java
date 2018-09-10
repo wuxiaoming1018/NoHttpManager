1dafc17b0e9aeb79e59cfdb2b9282f0f5f0dbd69
1398565099@qq.com <wmm911018!>
2018-09-10 19:05:26 +0800

    update

--- app/src/main/java/com/android/ming/nohttpmanager/MainActivity.java
+++ app/src/main/java/com/android/ming/nohttpmanager/MainActivity.java
@@ -3,6 +3,8 @@ package com.android.ming.nohttpmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+//学习资料  https://www.jianshu.com/p/9332167077b9
 public class MainActivity extends AppCompatActivity {
 
     @Override
